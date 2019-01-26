45ba9e7614a8cebc7780430741dbd3d037783308
Antonio Vivace <avivace4@gmail.com>
2019-01-26 12:06:51 +0100

    Enable @vuepress/last-updated plugin

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  base:'/docs/', // We are serving on coderbot.org/docs, so set the baseURL accordingly
+  // We are serving on coderbot.org/docs, so set the baseURL accordingly
+  // This variable is exposed on MD, HTML and Vue components as $withBase
+  // E.g. <img :src="$withBase('/foo.png')" alt="foo">
+  base: '/docs/',
+  themeConfig: {
+    // Based on last git commit, offered by @vuepress/plugin-last-updated
+    lastUpdated: 'Ultimo aggiornamento', // string | boolean
+  }
 }
