a317ee8ee2beb1667048799de6f164355ef0a32d
Previtera Roberto <roberto.previtera@gmail.com>
2019-02-12 18:53:34 +0100

    changed base url

--- pages/.vuepress/config.js
+++ pages/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  locales:{
-  '/': {
-      lang: 'it-IT', // this will be set as the lang attribute on <html>
-      title: 'Documentazione di CoderBot',
-      description: 'Guide e istruzioni per l\'utilizzo di CoderBot'
+  locales: {
+    "/": {
+      lang: "it-IT", // this will be set as the lang attribute on <html>
+      title: "Documentazione di CoderBot",
+      description: "Guide e istruzioni per l'utilizzo di CoderBot"
     }
   },
   plugins: [
@@ -13,17 +13,17 @@ module.exports = {
   // We are serving on coderbot.org/docs, so set the baseURL accordingly
   // This variable is exposed on MD, HTML and Vue components as $withBase
   // E.g. <img :src="$withBase('/foo.png')" alt="foo">
-  base: '/docs/',
+  base: "/",
   themeConfig: {
     sidebar: [
-      ['/', 'Indice'],
-      ['/kit/', 'Montaggio'],
-      ['/description/', 'Elenco delle parti'],
-      ['/manual/', 'Configurazione'],
-      ['/usermanual/', 'Manuale d\'uso'],
-      ['/guides/', 'Guide']
+      ["/", "Indice"],
+      ["/kit/", "Montaggio"],
+      ["/description/", "Elenco delle parti"],
+      ["/manual/", "Configurazione"],
+      ["/usermanual/", "Manuale d'uso"],
+      ["/guides/", "Guide"]
     ],
     // Based on last git commit, offered by @vuepress/plugin-last-updated
-    lastUpdated: 'Ultimo aggiornamento', // string | boolean
+    lastUpdated: "Ultimo aggiornamento" // string | boolean
   }
-}
+};
